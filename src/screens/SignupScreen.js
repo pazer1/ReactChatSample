108555a26740a551268f8f4af22a9ad6c16e2fed
GoJunseong <31427602+pazer1@users.noreply.github.com>
2020-06-10 20:05:50 +0900

    temp

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,14 +1,15 @@
 import React, {useState, useContext} from 'react';
-import {View, StyleSheet} from 'react-native';
+import {View, StyleSheet, PermissionsAndroid} from 'react-native';
 import {Title, IconButton} from 'react-native-paper';
 import FormInput from '../components/FormInput';
 import FormButton from '../components/FormButton';
 import {AuthContext} from '../navigation/AuthProvider';
-
+import ImagePicker from 'react-native-image-picker';
 export default function SignupScreen({navigation}) {
   const {register} = useContext(AuthContext);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [avartar, setAvatar] = useState('../images/profile-icon-png.png');
 
   const styles = StyleSheet.create({
     container: {
@@ -31,10 +32,60 @@ export default function SignupScreen({navigation}) {
       marginTop: 10,
     },
   });
+  const options = {
+    title: 'Select Avatar',
+    storageOptions: {
+      skipBackup: true,
+      path: 'images',
+    },
+  };
+
+  const requestCameraPermission = async () => {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.CAMERA,
+        PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
+        {
+          title: '카메라 권한',
+          message:
+            '프로필 사진등록을 위해선' +
+            '카메라 권한과 파일읽기 권한이 필요합니다.',
+          buttonNegative: 'Cancel',
+          buttonPositive: 'OK',
+        },
+      );
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        ImagePicker.showImagePicker(options, (response) => {
+          console.log('Response = ', response);
+          if (response.didCancel) {
+            console.log('User cancelled image picker');
+          } else if (response.error) {
+            console.log('ImagePicker error ', response.error);
+          } else if (response.CustomButton) {
+            console.log('User tapped custom button: ', response.customButton);
+          } else {
+            const source = {uri: response.uri};
+            setAvatar({
+              avartar: source,
+            });
+          }
+        });
+      } else {
+        console.log('Camera permission denied');
+      }
+    } catch (err) {
+      console.warn(err);
+    }
+  };
 
   return (
     <View style={styles.container}>
       <Title style={styles.titleText}>Register to chat</Title>
+      <IconButton
+        icon={require(avartar)}
+        size={50}
+        onPress={requestCameraPermission}
+      />
       <FormInput
         labelName="Email"
         value={email}
